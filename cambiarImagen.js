f58cbe1b921bc6372c652f519f876e4cc2184b9d
carlos lopez <colopez8@misena.edu.co>
2015-07-28 15:37:36 -0500

    conexionremota

---
+++ cambiarImagen.js
@@ -0,0 +1,62 @@
+var ultimaImagen = document.getElementById('messi').src;
+
+function cr7() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src= "./imagenes/cristiano.jpg";
+}
+function volver() {		
+	document.getElementById('messi').src =ultimaImagen;
+}
+function james() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src = "./imagenes/james.jpg";
+}
+function hazard() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src = "./imagenes/hazard.jpg";
+}
+function cuadrado(){
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src = './imagenes/cuadrado.jpg';
+}
+function neymar() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/neymar.jpg';
+}
+function roben() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/roben.jpg';
+}
+function falcao() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/falcao.jpg';
+}
+function ronaldinho() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/ronaldinho.jpg';}
+function bale() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/bale.jpg';}
+
+function zlatan() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/zlatan.jpg';}
+function iniesta() {
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/iniesta.jpg';}
+	function xavi() {
+	
+
+	ultimaImagen = document.getElementById('messi').src;
+	document.getElementById('messi').src ='./imagenes/xavi.jpg';}
+
+
+function fijarImagen(archivo) {		
+
+	var dirtemp = "./imagenes/"+archivo;
+	ultimaImagen = document.getElementById('messi').src=dirtemp;
+}
+
+
+
+
